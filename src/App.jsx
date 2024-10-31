f9c95b36e9d73f48cd15fd09b1244926557f7c0e
Rorisang Putu <rorisangputu@gmail.com>
2024-10-31 22:51:21 +0200

    new commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Pages
+import Home from "./pages/home";
+import Model from "./pages/model";
+//components
+import Header from "./components/Header";
+//Styles
+import "./App.scss";
 
-import './App.css'
 
 function App() {
-
+  const imageDetails = {
+    width: 524,
+    height: 650,
+  };
 
   return (
-    <>
-      s</>
-  )
+    <Router>
+      <Header />
+      <Route
+        render={({ location }) => (
+          <AnimatePresence initial={false} exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route
+                exact
+                path='/'
+                render={() => <Home imageDetails={imageDetails} />}
+              />
+              <Route
+                exact
+                path='/model/:id'
+                render={() => <Model imageDetails={imageDetails} />}
+              />
+            </Switch>
+          </AnimatePresence>
+        )}
+      />
+    </Router>
+  );
 }
 
-export default App
+export default App;
